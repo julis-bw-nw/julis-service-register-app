fcd610ed53f2ee0ba791a78e80f1318ae15f62fe
mb175 <moritz.megerle@icloud.com>
2022-01-17 01:09:48 +0100

    feat: WIP jquery dosent work !

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,11 +3,9 @@ require('./scss/style.scss');
 
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
-import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
-import '@shoelace-style/shoelace/dist/components/rating/rating.js';
-import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js'
-import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js'
+import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
+
 
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
-
+const webpack = require("webpack");
 
 module.exports = {
     entry: './src/app.js',
     output: {
         path: __dirname + '/dist/build',
-        filename: "main.js"
+        filename: "app.js"
     },
     devServer: {
         static: {
@@ -33,6 +33,10 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         }),
@@ -42,7 +46,11 @@ module.exports = {
                 {
                     from: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
                     to: path.resolve(__dirname, 'dist/build/shoelace/assets')
-                }
+                },
+                {
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'dist/build/assets')
+                },
             ]
         })
     ]
