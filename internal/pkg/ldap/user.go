1084cb1d2dd0b37fe0acde6142365c891aecc31b
Hendrik Schlehlein <hendrik.schlehlein@gmail.com>
2022-03-01 19:36:54 +0100

    feat: add lldap create user method

--- internal/pkg/ldap/user.go
+++ internal/pkg/ldap/user.go
@@ -1,10 +1,17 @@
 package ldap
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/julis-bw-nw/julis-service-register-app/internal/app/register/user"
 )
+
+var ErrResponseNotOK = errors.New("response is not 200 OK")
 
 type DataService interface {
 	UsersToRegister() ([]user.User, error)
@@ -15,24 +22,48 @@ type Service struct {
 	Client      http.Client
 }
 
-func (s Service) RegisterUser(user user.User) error {
-	return nil
-}
-
-func (s Service) createUser(user user.User) error {
+func (s Service) CreateUser(url string, user user.User) error {
 	graphql := map[string]interface{}{
 		"variables": map[string]interface{}{
 			"user": map[string]interface{}{
-				"id":          "hschlehlein",
-				"email":       "hendrik.schlehlein@julis.de",
-				"displayName": "Hendrik Schlehlein",
-				"firstName":   "Hendrik",
-				"lastName":    "Schlehlein",
+				"id":          userId(user),
+				"email":       user.Email,
+				"displayName": displayName(user),
+				"firstName":   user.FirstName,
+				"lastName":    user.LastName,
 			},
 		},
 		"query":         "mutation CreateUser($user: CreateUserInput!) {\n  createUser(user: $user) {\n    id\n    creationDate\n  }\n}\n",
 		"operationName": "CreateUser",
 	}
 
-	req, err := http.NewRequest("")
+	bb, err := json.Marshal(graphql)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(bb))
+	if err != nil {
+		return err
+	}
+
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return ErrResponseNotOK
+	}
+
+	return nil
+}
+
+func userId(user user.User) string {
+	return strings.ToLower(string(user.FirstName[0]) + user.LastName)
+}
+
+func displayName(user user.User) string {
+	return fmt.Sprintf("%s %s", user.FirstName, user.LastName)
 }
